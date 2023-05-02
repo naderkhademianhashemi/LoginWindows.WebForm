5e26a65b3d99aad1256fd0c42a9a91eb982031b8
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-02 08:48:28 +0430

    Add project files.

---
+++ LoginWindows.WebForm/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginWindows.WebForm
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var principal = (WindowsPrincipal)User;
+            var x = Request.IsAuthenticated;
+            var identity = (WindowsIdentity)User.Identity;
+            var token = identity.Token;
+        }
+    }
+}
